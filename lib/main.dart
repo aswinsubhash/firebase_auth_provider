6bce8d4ad6ea7943c3139f7555c8329da2f739ba
Aswin S <aswinofficial3@gmail.com>
2023-08-13 11:50:12 +0530

    All Firebase Settings are done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_auth_provider/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp()); 
 }
 
 class MyApp extends StatelessWidget {
