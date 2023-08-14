d00adf2c50963797d0be44803559ec8c1a1e1484
Aswin S <aswinofficial3@gmail.com>
2023-08-14 17:28:35 +0530

    Auth Repository completed

--- lib/models/custom_error.dart
+++ lib/models/custom_error.dart
@@ -1,19 +1,19 @@
 import 'package:equatable/equatable.dart';
 
-class CustomerError extends Equatable {
+class CustomError extends Equatable {
   final String code;
   final String message;
   final String plugin;
-  const CustomerError({
-    required this.code,
-    required this.message,
-    required this.plugin,
+  const CustomError({
+    this.code = '',
+    this.message = '',
+    this.plugin = '',
   });
-  
 
   @override
   List<Object> get props => [code, message, plugin];
 
   @override
-  String toString() => 'CustomerError(code: $code, message: $message, plugin: $plugin)';
+  String toString() =>
+      'CustomerError(code: $code, message: $message, plugin: $plugin)';
 }
