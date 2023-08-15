3e2978f135960e9bc2e2294e98010abfdf4498a9
Aswin S <aswinofficial3@gmail.com>
2023-08-15 10:42:29 +0530

    Injected AuthRepo,StreamProvider & AuthProvider

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_auth_provider/pages/home_page.dart';
+import 'package:firebase_auth_provider/pages/siginin_page.dart';
+import 'package:firebase_auth_provider/providers/auth/auth_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SplashPage extends StatelessWidget {
   static const String routeName = '/';
@@ -6,6 +10,17 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final authState = context.watch<AuthProvider>().state;
+
+    if (authState.authStatus == AuthStatus.authenticated) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        Navigator.pushNamed(context, HomePage.routeName);
+      });
+    } else if (authState.authStatus == AuthStatus.unauthenticated) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        Navigator.pushNamed(context, SigninPage.routeName);
+      });
+    }
     return const Scaffold(
       body: Center(
         child: CircularProgressIndicator(),
