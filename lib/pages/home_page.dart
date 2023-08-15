5e87d2768807fc3e4373275b488a8bea44ef1c94
Aswin S <aswinofficial3@gmail.com>
2023-08-15 17:18:20 +0530

    Home page, signout done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth_provider/pages/profile_page.dart';
+import 'package:firebase_auth_provider/providers/auth/auth_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   static const String routeName = '/home';
@@ -11,11 +14,60 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return  WillPopScope(
+    return WillPopScope(
       onWillPop: () async => false,
-      child: const Scaffold(
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          centerTitle: true,
+          title: const Text('Home'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                context.read<AuthProvider>().signout();
+              },
+              icon: const Icon(Icons.logout),
+            ),
+            IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const ProfilePage(),
+                  ),
+                );
+              },
+              icon: const Icon(Icons.account_circle),
+            )
+          ],
+        ),
         body: Center(
-          child: Text('Home'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/flutter_favorite.png',
+                    width: 100,
+                  ),
+                  const SizedBox(width: 20.0),
+                  const Text(
+                    'Provider',
+                    style: TextStyle(fontSize: 42.0),
+                  )
+                ],
+              ),
+              const SizedBox(height: 20.0),
+              const Text(
+                'Provider is an awesom\nstate management library\nfor flutter!',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 24.0),
+              )
+            ],
+          ),
         ),
       ),
     );
